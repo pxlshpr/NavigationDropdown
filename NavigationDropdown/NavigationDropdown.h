3b762574808ce70e49621cce65ec6c716fad9792
Ahmed Khalaf <pxlshpr@gmail.com>
2017-08-04 09:33:40 +0800

    initial commit

---
+++ NavigationDropdown/NavigationDropdown.h
@@ -0,0 +1,19 @@
+//
+//  NavigationDropdown.h
+//  NavigationDropdown
+//
+//  Created by pxlshpr on 4/8/17.
+//  Copyright © 2017 pxlshpr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NavigationDropdown.
+FOUNDATION_EXPORT double NavigationDropdownVersionNumber;
+
+//! Project version string for NavigationDropdown.
+FOUNDATION_EXPORT const unsigned char NavigationDropdownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavigationDropdown/PublicHeader.h>
+
+
